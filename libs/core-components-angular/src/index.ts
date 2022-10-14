0299f9b7d0d35ac8d9c2040d63ca1f39a8ee0bf0
Robert Parker <robert.parker@alertlogic.com>
2022-10-14 15:41:20 +0100

    add explicit component exports

--- libs/core-components-angular/src/index.ts
+++ libs/core-components-angular/src/index.ts
@@ -1,1 +1,8 @@
+export {
+    MyComponent,
+    OtherComponent,
+    AldBadge,
+    AldButton,
+    AldIcon
+} from './generated/directives/proxies';
 export * from './lib/core-components-angular.module';
